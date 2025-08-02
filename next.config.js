6537cbcae67a5fd3c217c43f1ea62a3979cb671d
real-vercel <tiki03078@gmail.com>
2025-08-02 09:58:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  }
+  // no need for experimental.appDir in 13.5+
 };
 
 module.exports = nextConfig;
