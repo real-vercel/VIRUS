859fabd4603929af37b9473786dbe43a276db3b2
real-vercel <tiki03078@gmail.com>
2025-08-02 10:51:52 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import '#/styles/globals.css';
 
 import db from '#/lib/db';
-import Byline from '#/ui/byline';
-import { GlobalNav } from '#/ui/global-nav';
 import { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
@@ -39,17 +37,18 @@ export default function RootLayout({
         className={`overflow-y-scroll bg-gray-950 font-sans ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="fixed top-0 z-10 flex w-full flex-col border-b border-gray-800 bg-black lg:bottom-0 lg:z-auto lg:w-72 lg:border-r lg:border-b-0 lg:border-gray-800">
-          <GlobalNav items={demos} />
+          
         </div>
 
         <div className="lg:pl-72">
           <div className="mx-auto mt-12 mb-24 max-w-4xl -space-y-[1px] lg:px-8 lg:py-8">
             {children}
 
-            <Byline />
+            
           </div>
         </div>
       </body>
     </html>
   );
 }
+
